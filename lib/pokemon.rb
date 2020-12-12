c96313f4fffe848d5d3f68a43efb0b07e2b86f99
Michael Fuget <michaelfuget@gmail.com>
2020-12-12 18:19:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,10 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
-  def initialize(id, name, type, db)
-    @@id = id 
-    @name = name 
-    @type = type 
-    @db =db 
+  def initialize(data)
+    @@id = data[:id] 
+    @name = data[:name] 
+    @type = data[:type]
+    @db = data[:db] 
   end 
   
   
